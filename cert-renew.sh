5151f91b3c7704cb4f0a13c3ab1a402e43a3e158
moshthepitt <kelvin@jayanoris.com>
2015-12-17 10:54:05 +0300

    include account_key, email and server reload

--- cert-renew.sh
+++ cert-renew.sh
@@ -4,7 +4,9 @@ cd /srv/cert/EXAMPLE.COM
 . /opt/simp_le/venv/bin/activate
 simp_le \
   -d EXAMPLE.COM:/var/www/static/EXAMPLE.COM/documentroot \
-  -f key.pem -f cert.pem -f fullchain.pem
+  -f account_key.json -f key.pem -f cert.pem -f fullchain.pem \
+  --email kelvin@jayanoris.com \
+  && service nginx reload
 
 # set up cron job:
 # 43 1 * * * /srv/bin/cert-renew.sh || true
