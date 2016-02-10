774fb91fc59eafb9f65fae5f9ad74f38d33cf6bf
Kelvin Jayanoris <moshthepitt@users.noreply.github.com>
2016-02-10 10:42:20 +0300

    Update cert-renew.sh
    add www certificate

--- cert-renew.sh
+++ cert-renew.sh
@@ -4,6 +4,7 @@ cd /srv/cert/EXAMPLE.COM
 . /opt/simp_le/venv/bin/activate
 simp_le \
   -d EXAMPLE.COM:/var/www/static/EXAMPLE.COM/documentroot \
+  -d www.EXAMPLE.COM:/var/www/static/EXAMPLE.COM/documentroot \
   -f account_key.json -f key.pem -f cert.pem -f fullchain.pem \
   --email kelvin@jayanoris.com \
   && service nginx reload
